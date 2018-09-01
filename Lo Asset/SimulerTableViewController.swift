b011adc30358bfae97f4285272415c52406df4f1
DatavenueLiveObjects <liveobjects.contact@orange.com>
2018-09-01 05:13:39 +0200

    Ressources View

---
+++ Lo Asset/SimulerTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SimulerTableViewController.swift
+//  Lo Asset
+//
+//  Created by Moustapha Kebe on 01/09/2018.
+//  Copyright © 2018 Orange. All rights reserved.
+//
+
+import UIKit
+
+class SimulerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+
+}
