aa40e716c430ed0d91e9d1b76514b75c423891d3
datavenueliveobjects <liveobjects.contact@orange.com>
2018-09-05 16:30:04 +0200

    Mergeeee

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks! # Add this if you are targeting iOS 8+ or using Swift
+target 'Lo Asset' do
+	pod 'MQTTClient'
+	pod 'CocoaMQTT'
+end
---
+++ Pods/Target Support Files/Pods-Lo Asset/Pods-Lo Asset-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lo_Asset : NSObject
+@end
+@implementation PodsDummy_Pods_Lo_Asset
+@end
